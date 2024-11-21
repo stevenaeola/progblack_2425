6fc39f9db66e9487c3442aa739b96f2f386b281c
dcs0spb <s.p.bradley@durham.ac.uk>
2024-11-21 16:50:09 +0000

    Initial express server with ignored modules

---
+++ examples/express/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+app.get('/random/:max', function(req, resp){
+    max = parseInt(req.params.max)
+    rand = Math.floor(Math.random()*max) +1
+    console.log('Max via url is ' + max + ' rand is ' + rand)
+    resp.send('' + rand)
+  })
+
+app.get('/r', function(req, resp){
+    max = parseInt(req.query.max)
+    rand = Math.floor(Math.random()*max) +1
+    console.log('Max via query is ' + max + ' rand is ' + rand)
+    resp.send('' + rand)
+  })
+
+app.listen(8090)
