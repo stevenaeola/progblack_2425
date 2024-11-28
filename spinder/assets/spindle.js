9ff2c9f37c7dabf28795dc9557ec7e9ae6fb96c1
dcs0spb <s.p.bradley@durham.ac.uk>
2024-11-28 16:13:28 +0000

    sorting out ignored files

--- spinder/assets/spindle.js
+++
@@ -1,9 +0,0 @@
-let spider_images = document.querySelectorAll("img.spider")
-
-for(let img of spider_images){
-    img.classList.add("invisible")
-    let clickable_parent = img.parentElement.parentElement.parentElement
-    clickable_parent.addEventListener("click", () => {
-        img.classList.remove("invisible")
-    })
-}
